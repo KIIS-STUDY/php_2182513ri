1a232d21223d2dc35b5a283a4b9c9868e45e8f0c
kiis-2182513 <kiis@aisic.jp>
2020-07-13 17:37:18 +0900

    7/13日 staff ファイル 完成

--- staff/staff_add_done.php
+++ staff/staff_add_done.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>ろくまる農園</title>
-    </head>
+<head>
+<meta charset="utf-8">
+<title>ろくまる農園</title>
+</head>
 <body>
 
 <?php
@@ -11,31 +11,31 @@
 try
 {
 
-$staff_name=$_POST['name'];
-$staff_pass=$_POST['pass'];
+    $staff_name = $_POST['name'];
+    $staff_pass = $_POST['pass'];
 
-$staff_name=htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
-$staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+    $staff_name=htmlspecialchars($staff_name, ENT_QUOTES,'UTF-8');
+    $staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
 
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+    $user='root';
+    $password='';
+    $dbh=new PDO($dsn,$user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql='INSERT INTO mst_staff(name,password) VALUES (?,?)';
-$stmt=$dbh->prepare($sql);
-$data[]=$staff_name;
-$data[]=$staff_pass;
-$stmt->execute($data);
+    $sql='INSERT INTO mst_staff(name,password)VALUES(?,?)';
+    $stmt=$dbh->prepare($sql);
+    $data[]=$staff_name;
+    $data[]=$staff_pass;
+    $stmt->execute($data);
 
-$dbh=null;
+    $dbh=null;
 
-print$staff_name;
-print'さんを追加しました。<br />';
+    print $staff_name;
+    print 'さんを追加しました。<br/>';
 
 }
-catch(Exception $e)
+ catch(Exception $e)
 {
     print'ただいま障害により大変ご迷惑をお掛けしております。';
     exit();
@@ -46,3 +46,4 @@ catch(Exception $e)
 <a href="staff_list.php">戻る</a>
 
 </body>
+</html>
